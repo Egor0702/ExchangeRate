6453c6858cd3e986c07b5be9913cefaf40c0a78b
Egor <65344158+Egor0702@users.noreply.github.com>
2022-03-28 14:30:01 +0300

    Add new currency

--- app/src/main/java/com/example/exchangerate/domain/Rates.kt
+++ app/src/main/java/com/example/exchangerate/domain/Rates.kt
@@ -5,17 +5,53 @@ import com.example.exchangerate.domain.core.Failure
 import com.google.gson.annotations.SerializedName
 
 class Rates (
+    val EUR: Double,
+    val USD : Double
     val AED: Double,
-    var AFN : Double,
-    var TRY : Double){
+    val AFN : Double,
+    val TRY : Double,
+    val NOK : Double,
+    val SEK : Double,
+    val AUD : Double,
+    val CAD : Double,
+    val CHF : Double,
+    val CNY : Double,
+    val GBP : Double,
+    val JPY : Double,
+    val BRL : Double,
+    val MXN : Double,
+    val ISK : Double,
+    val GEL : Double,
+    val KWD : Double,
+    val BHD : Double,
+    val KYD : Double,
+    val AZN : Double
+){
 
     fun getMap(): BusLogic<Failure, MutableMap<String,Double>> {
         var mapRates = mutableMapOf<String, Double>()
         mapRates.apply {
+            put("Евро", EUR)
+            put ("Доллар США", USD)
             put("Дирхам ОАЭ", AED)
             put ("Афганский афгани", AFN)
             put ("Турецкая лира", TRY)
-
+            put ("Норвежская крона", NOK)
+            put ("Шведская крона", SEK)
+            put("Австралийский доллар", AUD)
+            put("Канадский доллар", CAD)
+            put ("Швейцарский франк", CHF)
+            put ("Китайский юань", CNY)
+            put("Фунт стерлингов", GBP)
+            put ("Иена", JPY)
+            put ("Бразильский реал", BRL)
+            put ("Мексиканское песо", MXN)
+            put ("Исландская крона", ISK)
+            put ("Грузинский лари", GEL)
+            put ("Кувейтский динар", KWD)
+            put ("Бахрейнский динар", BHD)
+            put ("Доллар Каймановых островов", KYD)
+            put ("Азербайджанский манат", AZN)
         }
         return BusLogic.Rightable(mapRates)
     }
